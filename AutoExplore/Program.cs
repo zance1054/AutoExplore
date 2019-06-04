26a04940bba6713921307e0c73007e3242927b02
Alec <32371145+zance1054@users.noreply.github.com>
2019-06-04 14:08:34 -0700

    Port
    specified specific port so can run on other machine local hosts

--- AutoExplore/Program.cs
+++ AutoExplore/Program.cs
@@ -19,6 +19,8 @@ namespace AutoExplore
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseStartup<Startup>()
+                .UseUrls("http://localhost:44384/");
+
     }
 }
